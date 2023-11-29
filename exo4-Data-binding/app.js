bbb0b186981373c898ddf7238fddc933b30be3a6
GentilleauF <francois.gentilleau@gmail.com>
2023-11-29 11:39:35 +0100

    Exo4 databinding v2

--- exo4-Data-binding/app.js
+++ exo4-Data-binding/app.js
@@ -5,7 +5,7 @@ Vue.createApp({
 data() {
     return {
         nom: "Fry",
-        age: 28,
+        age: 25,
         nbrFetiche: Math.random(),
         monImage : "./fry.png"
 
@@ -15,6 +15,23 @@ data() {
 methods: {
     plusVieux () {
         return "Mon age dans 10 ans: " + (this.age+10) + ' ans'
+    },
+
+    changeName() {
+        console.log('ok');
+        this.nom = nameInput.value
+    },
+
+    decrement() {
+        if(this.age >1){
+            this.age--
+        } else {
+            this.age==1
+        }
+    },
+    
+    increment() {
+        this.age++
     }
 
 }
