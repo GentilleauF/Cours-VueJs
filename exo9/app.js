94d75cfb63128773cb8826ed19a8a019fdd91377
GentilleauF <francois.gentilleau@gmail.com>
2023-12-04 11:59:30 +0100

    exo9

---
+++ exo9/app.js
@@ -0,0 +1,27 @@
+
+
+Vue.createApp({
+  data() {
+    return {
+      selectCard1: false,
+      selectCard2: false,
+    };
+  },
+
+  methods: {
+    selectCard(uneCard) {
+      console.log(uneCard);
+      if (uneCard ==1){
+        this.selectCard1 = !this.selectCard1;
+      } else if(uneCard ==2){
+        this.selectCard2 = !this.selectCard2;
+      }
+    }
+  },
+
+}).mount("#app");
+
+
+
+
+
